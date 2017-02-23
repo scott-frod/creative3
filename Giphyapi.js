2990a5a87f36c2dcae98ae22fe50375be4c6032d
Ben Horrocks <benjamin.horrocks@byu.edu>
2017-02-22 23:14:45 -0700

    Added more API functionality; added GIF filters

--- Giphyapi.js
+++ Giphyapi.js
@@ -4,12 +4,29 @@ app.controller('FirstController', function ($scope, $http) {
     $scope.giftwo = "http:\/\/media2.giphy.com\/media\/iuHaJ0D7macZq\/giphy.gif";
     $scope.StartBattle = function () {
         var searchterm=$scope.Subject;
-        console.log(searchterm);
-        $http.get('http://api.giphy.com/v1/gifs/search?q='+searchterm+'&api_key=dc6zaTOxFJmzC').
+//        for(var i=0;i<16;i++){
+//            $http.get('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag='+searchterm + '&rating=pg').
+//            then(function (response) {
+//                $scope.gifone=response.data.data.image_url;
+//            });
+//            
+//        }
+        $http.get('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag='+searchterm + '&rating=pg').
         then(function (response) {
-            var i=1;
-            console.log(response.data.data[i].images.original.url);
-            $scope.gifone=response.data.data[i].images.original.url;
+            console.log(response.data.data.image_mp4_url);
+            $scope.gifone=response.data.data.image_url;
+        });
+        $http.get('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=' + searchterm +'&rating=pg').
+        then(function (response) {
+            console.log(response.data.data.image_mp4_url);
+            $scope.giftwo=response.data.data.image_url;
         });        
+    };
+    
+    $scope.PickOne = function () {
+        console.log("You Picked GIF number 1!");
+    };
+    $scope.PickTwo = function () {
+        console.log("You Picked GIF number 2!");
     };
 });
